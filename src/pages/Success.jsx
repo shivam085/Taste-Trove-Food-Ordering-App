323b3fc3c25ac7484fbde357f499594aa7bf893f
shivam085 <shivamkrcvam@gmail.com>
2025-07-09 13:30:32 +0530

    Updated

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Navigate } from 'react-router';
 import { PropagateLoader } from "react-spinners";
 
 const Success = () => {
@@ -9,16 +10,29 @@ const Success = () => {
     }, 3000);
   }, [])
 
+
+  const [newloading, setNewLoading] = useState(true)
+  useEffect(() => {
+    setTimeout(() => {
+      setNewLoading(false)
+    }, 6000);
+    
+  }, [])
+
+
+
   return (
 
     <div className='flex flex-col items-center justify-center h-screen'>
       {
         loading ? <PropagateLoader color="#36d7b7" />
           :
+          newloading ?
           (<div>
             <h2 className='text-3xl font-semibold mb-4 '>Order Successful!</h2>
             <p>Your odrer has been successfully placed! </p>
-          </div>)
+          </div>) 
+          : <Navigate to="/" />
       }
 
     </div>
